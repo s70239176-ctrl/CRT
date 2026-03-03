0fa53f6f11b860b6ad3b3e03932f9981b6b5fc29
Ernest rewer <s70239176@gmail.com>
2026-03-03 21:33:01 +0100

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,16 @@
+import { Blockchain } from '@btc-vision/btc-runtime/runtime';
+import { revertOnError } from '@btc-vision/btc-runtime/runtime/abort/abort';
+import { LiquidityLocker } from './contract/LiquidityLocker';
+
+// DO NOT TOUCH THIS.
+Blockchain.contract = () => {
+    return new LiquidityLocker();
+};
+
+// VERY IMPORTANT
+export * from '@btc-vision/btc-runtime/runtime/exports';
+
+// VERY IMPORTANT
+export function abort(message: string, fileName: string, line: u32, column: u32): void {
+    revertOnError(message, fileName, line, column);
+}
